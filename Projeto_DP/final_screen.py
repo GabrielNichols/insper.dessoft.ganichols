c5397a8c27704fe27cb4296f753f12badea92b53
Gabriel <gabrielrn@al.insper.edu.br>
2021-08-11 12:02:42 -0300

    colocando pontuação na tela final

--- Projeto_DP/final_screen.py
+++ Projeto_DP/final_screen.py
@@ -9,6 +9,13 @@ def final_screen(screen,score):
     
     clock = pygame.time.Clock()
     background = pygame.image.load(path.join(IMG_DIR, 'final.png')).convert()
+    
+
+    text_surface = (pygame.font.Font(path.join(FNT_DIR, 'inicial.ttf'), 31)).render("{:08d}".format(score), True, BLACK)
+
+    text_rect = text_surface.get_rect()
+    text_rect = (22 ,  375)
+
     background_rect = background.get_rect()
 
     running = True
@@ -32,7 +39,7 @@ def final_screen(screen,score):
 
         # A cada loop, redesenha o fundo e os sprites
         screen.blit(background, background_rect)
-
+        screen.blit(text_surface, text_rect)
         # Depois de desenhar tudo, inverte o display.
         pygame.display.flip()
 
